a99ab2141d7a8cdb132fe67709f349523a72cd96
Darrell Weaver <darrellweaverjr@gmail.com>
2020-01-07 10:58:56 -0800

    elio initial template

---
+++ _creds.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Credentials for the Dynamix User
+ */
+
+$api_email    = "michael@newepochsoftware.com";
+$dynamixkey   = "ygAx1_95a8a3d556201f888d34d27f11dfc8235ec24fb1";
+$glowfishkey  = "k2jKv_1022838621137371982c5dbb17255febdaa3e1cc";
+$templates = array(
+  "index"     =>  "lander",
+  "policy"    =>  "policy",
+  "privacy"   =>  "privacy",
+  "thank-you" =>  "thank-you"
+);
