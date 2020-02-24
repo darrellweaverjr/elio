5871a92802c5e654d3558bf693b98dc8e65edc01
Darrell Weaver <darrellweaverjr@gmail.com>
2020-02-24 11:14:35 -0800

    removed drinks from footer

--- thank-you.php
+++ thank-you.php
@@ -133,9 +133,9 @@ require dirname(__FILE__).'/_dynamix.php';
       </div>
     </div>
 
-    <div class="gallery-sect" data-xid="drinks">
-      <?= $profile->xid("drinks") ?>
-    </div>
+    <!-- <div class="gallery-sect" data-xid="drinks"> -->
+      <?= /* $profile->xid("drinks") */ ?>
+    <!-- </div> -->
 
     <section class="footer" id="footer">
       <div class="footer-content" data-xid="footer-content">
